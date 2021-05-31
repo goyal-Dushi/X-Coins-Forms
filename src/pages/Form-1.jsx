bc9b0a3442eae3c7e10326caa2a8c0df0c36214e
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-05-31 22:29:49 +0530

    updated formik version and styles

--- src/pages/Form-1.jsx
+++ src/pages/Form-1.jsx
@@ -1,5 +1,5 @@
-import { useFormik } from "formik";
-import React from "react";
+import { ErrorMessage, Field, Formik} from "formik";
+import React, { useState } from "react";
 import Warning from "../components/Warning";
 import ClockIcon from "../images/icons/clock-icon.svg";
 import Label from "../components/Label";
@@ -7,158 +7,113 @@ import SubmitButton from "../components/SubmitBtn";
 import "../sass/forms.scss";
 import ReCaptchaV2 from "react-google-recaptcha";
 import { useHistory } from "react-router";
-
-const validate =(values) => {
-    const errors = {};
-
-    if(!values.firstName){
-        errors.firstName = "Required";
-    }
-    else if(values.firstName.length > 15){
-        errors.firstName = "Must be less than 15 characters";
-    }
-
-    if(!values.lastName){
-        errors.lastName = "Required";
-    }
-    else if(values.lastName.length > 10){
-        errors.lastName = "Must be less than 10 characters";
-    }
-
-    if(!values.email){
-        errors.email = "Required";
-    }
-    else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
-        errors.email = "Incorrect Email address provided";
-    }
-
-    if(!values.password){
-        errors.password = "Required";
-    }
-    else if(values.password.length < 5){
-        errors.password = "Password Length too short";
-    }
-    if(!values.terms){
-        errors.terms = "Please accept the terms and Conditions"
-    }
-
-    return errors;
-}
+import * as Yup from "yup";
 
 function Form1()
 {
     const history = useHistory();
-    const formik = useFormik({
-        initialValues : {
+    const [firstNameClass, setfirstNameClass] = useState('correct-input');
+    const [lastNameClass, setlastNameClass] = useState('correct-input');
+    const [emailClass, setemailClass] = useState('correct-input');
+    const [passwordClass, setpasswordClass] = useState('correct-input');
+    return(     
+        <Formik
+        initialValues = {{
             firstName : '',
             lastName : '',
             email : '',
             password : '',
             terms : false
-        },
-        validate,
-        onSubmit: (values) => {
+        }}
+        validationSchema = { Yup.object({
+            firstName: Yup.string()
+            .max(12, "Must be 12 characters or less")
+            .required("Required Field"),
+            lastName : Yup.string()
+            .max(12, "Must be 12 characters or less")
+            .required("Required Field"),
+            email : Yup.string()
+            .email("Invalid email ID provided")
+            .required("Required Field"),
+            password : Yup.string()
+            .max(15, "Must be of max 15 characters")
+            .min(4, "Cannot be less than 4 characters")
+            .required("Required Field")
+            .matches("(?=.*[A-Z])", "Must contain atlease one UPPERCASE char")
+            .matches("(?=.*[a-z])","Must contain atleast one lowercase char")
+            .matches("(?=.*[-+_!@#$%^&*., ?])", "Must contain atleast one special character"),
+            terms: Yup.boolean()
+            .oneOf([true], "Must accept the terms and conditions first")
+        }) }
+        onSubmit = {(values) => {
             console.log(
                 "Form is successfully submitted :"+
                 values.firstName+" "+
                 values.lastName
-            );
+            );  
             history.push("/upd");
         }
-    });
-    
-    return(
-        <>
+    }
+    >
 
-        <Warning imgSrc={ClockIcon} altImg="Clock-Icon"
+         {formik => (
+         <>
+         <Warning imgSrc={ClockIcon} altImg="Clock-Icon"
                     height={72} 
                     content="Once submitted,your profile cannot be changed. 
                     Please ensure that your information is correct." 
         />
 
         <form onSubmit={formik.handleSubmit}>
+
+            <div className="input-container" >
             <Label title="First name*" labelFor="firstName" />
-            <input 
-            type="text" 
-            id="firstName"
-            name="firstName"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.firstName}
-            autoComplete="off"
-            placeholder="Enter First Name"
-            />
-            {formik.touched.firstName && formik.errors.firstName ? (
-             <div className="error-msg" > {formik.errors.firstName} </div>   
-            ): null}
+            <Field autocomplete="off" name="firstName" type="text" placeholder="Enter First Name" className={firstNameClass} />
+            {formik.errors.firstName && formik.touched.firstName ? setfirstNameClass('error-input') : setfirstNameClass('correct-input')}
+            <ErrorMessage style={{display:"block"}} name="firstName" className="error-msg" component="div" />
+            </div>
+            
+            <div className="input-container">
+            <Label labelFor="#lastName" title="Last name*" />
+            <Field autocomplete="off" name="lastName" type="text" placeholder="Enter Last Name" className={lastNameClass} />
+            {formik.errors.lastName && formik.touched.lastName ? setlastNameClass('error-input') : setlastNameClass('correct-input')}
+            <ErrorMessage name="lastName" className="error-msg" component="div" />
+            </div>
 
-            <Label labelFor="lastName" title="Last name*" />
-            <input 
-            type="text" 
-            id="lastName"
-            name="lastName"
-            autoComplete="off"
-            placeholder="Enter Last Name"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.lastName}
-            />
-            {formik.touched.lastName && formik.errors.lastName ? (
-             <div className="error-msg" > {formik.errors.lastName} </div>   
-            ): null}
+            <div className="input-container">
+            <Label title="Email address*" labelFor="#email" />
+            <Field autocomplete="off" type="text" name="email" placeholder="Enter Email ID" className={emailClass} />
+            {formik.errors.email && formik.touched.email ? setemailClass('error-input') : setemailClass('correct-input')}
+            <ErrorMessage name="email" component="div" className="error-msg" />
+            </div>
 
-            <Label title="Email address*" labelFor="email" />
-            <input 
-            type="email" 
-            name="email" 
-            id="email"
-            autoComplete="off"
-            placeholder="Enter Email ID"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-            />
-            {formik.touched.email && formik.errors.email ? (
-                <div className="error-msg" > {formik.errors.email} </div>
-            ): null}
-
+            <div className="input-container">
             <Label labelFor="password" title="Create Password" />
-            <input 
-            type="password" 
-            name="password" 
-            id="password"
-            placeholder="Enter a Strong Password"
-            onBlur={formik.handleBlur}
-            onChange={formik.handleChange}
-            value={formik.values.password} 
-            />
-            {formik.touched.password && formik.errors.password ? (
-                <div className="error-msg" > {formik.errors.password} </div>
-            ): null}
+            <Field name="password" placeholder="Enter Password" type="password" className={passwordClass} />
+            {formik.errors.password && formik.touched.password ? setpasswordClass('error-input') : setpasswordClass('correct-input')}
+            <ErrorMessage name="password" component="div" className="error-msg" />
+            </div>
 
+            <div className="conditional-container">
             <div className="conditional-box" >
-                <input 
-                type="checkbox" name="terms" id="terms" 
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.terms}
-                />
+                <Field type="checkbox" name="terms" />
                 <p>
                     By clicking «Continue» you agree to our <a href="#" className="links">Terms</a> ,<a href="#" className="links"> Cookies Policy </a> and 
                     <a href="#" className="links"> Privacy Policy </a>. You may receive email and SMS notifications from 
                     us and can opt out at any time.
                 </p>
             </div>
-            {formik.errors.terms && formik.touched.terms ? 
-            ( <div className="error-msg"> {formik.errors.terms} </div> )
-            : null}
-            
+            <ErrorMessage name="terms" component="div" className="error-msg" />
+            </div>
+
             <ReCaptchaV2 sitekey={process.env.REACT_APP_SITE_KEY} />
             <div style={{marginTop:40}}>
             <SubmitButton name="CONTINUE"  typeFor="submit" />
             </div>
         </form>
         </>
+     )}
+     </Formik>  
     );
 }
 
