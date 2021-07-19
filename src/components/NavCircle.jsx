bd5ccda7af4cc9ab230456d383faff5cfc62f6de
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-07-19 12:21:32 +0530

    added active class and props to toggle active color status

--- src/components/NavCircle.jsx
+++ src/components/NavCircle.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React, { memo } from "react";
 import "./navCircle.scss";
 import Label from "./Label";
 
-function NavCircle(props)
-{
-    return(
-
-        <div className="circle-wrapper">
-            <div className="circle">
-                {props.content}
-            </div>
-            <Label 
-            labelFor={props.content} 
-            title={props.labelHead}
-            style={{fontWeight:"bold"}} 
-            />
-        </div>
-    )
+function NavCircle(props) {
+  return (
+    <div className='circle-wrapper'>
+      <div className={props.status ? "circle activeCircle" : "circle"}>
+        {props.content}
+      </div>
+      <Label
+        labelFor={props.content}
+        title={props.labelHead}
+        style={{ fontWeight: "bold" }}
+      />
+    </div>
+  );
 }
 
-export default NavCircle;
+export default memo(NavCircle);
